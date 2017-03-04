31752da5455326d370d220a5b01f3b256c1ecdba
marcobotteri <marco.botteri@hotmail.com>
2017-03-04 18:07:40 +0100

    esercizio

--- es3pag154.cpp
+++ es3pag154.cpp
@@ -3,5 +3,22 @@ using namespace std;
 
 main()
 {
-
+int v[10];
+int numeri;
+int n;
+int i=0;
+do{
+cout<<"Quanti numeri vuoi inserire? (<=10) ";
+cin>>numeri;} while(numeri>10);
+for(i=1;i<=numeri;i++)
+{
+    cout<<"Inserire il "<<i<<" numero: ";
+    cin>>
+    v[i];
+}
+cout<<"I numeri inseriti nell'ordine inverso sono:"<<endl;
+for(i=numeri;i>0;i--)
+{
+    cout<<v[i]<<" ";
+}
 }
