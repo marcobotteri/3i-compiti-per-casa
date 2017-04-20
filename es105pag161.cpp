7b72205db5562d7ac270e2e721e8cf29ca392c33
marcobotteri <marco.botteri@hotmail.com>
2017-04-20 13:05:13 +0200

    SECONDO MODO

--- es105pag161.cpp
+++ es105pag161.cpp
@@ -6,12 +6,12 @@ char stringa_in[]={'c','i','a','o','\0'};
 string stringa_in2="Ciao mamma come va?";
 int numero_vocali=0;
 int i=0;
-while (stringa_in[i]!='\0'){
-    if(stringa_in[i]=='a' ||stringa_in[i]=='e' ||stringa_in[i]=='i' ||stringa_in[i]=='o' ||stringa_in[i]=='u' ||
-       stringa_in[i]=='A' ||stringa_in[i]=='E' ||stringa_in[i]=='I' ||stringa_in[i]=='O' ||stringa_in[i]=='U')
+while (stringa_in2[i]!='\0'){
+    if(stringa_in2[i]=='a' ||stringa_in2[i]=='e' ||stringa_in2[i]=='i' ||stringa_in2[i]=='o' ||stringa_in2[i]=='u' ||
+       stringa_in2[i]=='A' ||stringa_in2[i]=='E' ||stringa_in2[i]=='I' ||stringa_in2[i]=='O' ||stringa_in2[i]=='U')
         numero_vocali++;
     i++;
 }
-
+cout<<"Il numero di vocali e': "<<numero_vocali<<endl;
 return 0;
 }
