8d97f3b1ec0e6930b0d105d857944073e5167a9a
marcobotteri <marco.botteri@hotmail.com>
2017-04-20 12:36:17 +0200

    versione non furba

---
+++ es41pag156.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#define N 100
+using namespace std;
+
+int main(){
+int a[]={0, 7, -1, 0,5,4,10,1,9,2,-43,-2};
+int dim=15;
+int somme[15];
+
+for (int i=0; i< dim; i++)
+{
+    int appoggio=0;
+    for(j=i;j<dim;j++){
+        appoggio+=a[i]
+    }
+    somme[i]=appoggio;
+}
+
+}
