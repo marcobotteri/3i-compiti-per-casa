ced72e953229cccbcb75eb8e72bcf2a7333313b3
marcobotteri <marco.botteri@hotmail.com>
2017-09-18 13:06:29 +0200

    tyrty

---
+++ es14pag109.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int inizializza(int& tentativi_max, int& numero, bool& vittoria, int& tentativo)
+
+main() {
+int tentativi_max, tentativo, numero;
+bool vittoria;
+bool sconfitta;
+inizializza(tentativi_max, numero, vittoria);
+
+}
+
+int inizializza(int& tentativi_max, int& numero, bool& vittoria,int& tentativo){
+tentativi_max=5;
+vittoria=false;
+}
