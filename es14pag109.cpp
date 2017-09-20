613a521dcf96a4a1620f28b93d458584ab4e2aa4
marcobotteri <marco.botteri@hotmail.com>
2017-09-20 13:54:29 +0200

    correzione

--- es14pag109.cpp
+++ es14pag109.cpp
@@ -10,21 +10,23 @@ main() {
 int tentativi_max, tentativo, numero, num_segreto;
 bool vittoria, sconfitta;
 
+cout<<"Hai 10 tenattivi per indovinare il numero segreto"<<endl<<endl;
+
 inizializza(tentativi_max, tentativo, numero, vittoria, sconfitta, num_segreto);
 
 do{
 interazioneUtente(numero, tentativo);
 controlloVittoria(numero, num_segreto, vittoria);
 controlloSconfitta(tentativo, tentativi_max, vittoria, sconfitta);
-} while(vittoria||sconfitta);
+} while(!vittoria && !sconfitta);
 
 }
 
 
 void inizializza(int& tentativi_max, int& tentativo, int& numero, bool& vittoria, bool& sconfitta, int& num_segreto){
-tentativi_max=5;
+tentativi_max=10;
 tentativo=0;
-num_segreto=10;
+num_segreto=5;
 vittoria=false;
 sconfitta=false;
 }
@@ -39,7 +41,7 @@ int interazioneUtente(int& numero, int& tentativo) {
 bool controlloVittoria(int numero, int num_segreto, bool& vittoria){
     if(numero==num_segreto){
             vittoria=true;
-            cout<<"Hai indovinato il numero"<<endl;
+            cout<<endl<<"Hai indovinato il numero"<<endl;
     }
 
 }
@@ -50,6 +52,6 @@ bool controlloSconfitta(int tentativo, int tentativi_max, bool vittoria, bool& s
             sconfitta=true;
             cout<<"Hai perso"<<endl;
             }
-        else if (vittoria==false) cout<<endl<<"Ritenta"<<endl;
+        else if (vittoria==false) cout<<"Ritenta"<<endl;
 }
 
